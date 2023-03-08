0d990b434730c3ca3ab70afd4ebadcbdb68c1b32
Trishna-Nath <t.nathbd@gmail.com>
2023-03-08 07:20:18 -0800

    added debug stop

---
+++ js/debug-stop.js
@@ -0,0 +1,7 @@
+/*
+debus steps
+1. go to the live website.and try o reproduce the error.
+2.check the console of the live website to see any error / exception
+3.read exception throughly.in detail
+4.if the error is new to you:copy  the text of the error and search online
+*/
